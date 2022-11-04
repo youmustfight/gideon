1bbbd1fb1f0731200e238014f8a376cf4a37fe35
Mark <x@markthemark.com>
2022-11-03 21:32:56 -0400

    feat: sqlalchemy 1.4/2.0

--- backends/src/orm.py
+++ backends/src/orm.py
@@ -1,11 +1,10 @@
-from tortoise import Tortoise
 import env
 
 # DB CONFIGS
-TORTOISE_ORM = {
+ORM_CONFIG = {
     "connections": {
         # https://github.com/tortoise/tortoise-orm/blob/main/docs/databases.rst#db_url
-        "default": f"asyncpg://{env.env_get_database_app_user_name()}:{env.env_get_database_app_user_password()}@{env.env_get_database_app_host()}:{env.env_get_database_app_port()}/{env.env_get_database_app_name()}"
+        "default": f"postgresql+asyncpg://{env.env_get_database_app_user_name()}:{env.env_get_database_app_user_password()}@{env.env_get_database_app_host()}:{env.env_get_database_app_port()}/{env.env_get_database_app_name()}"
     },
     "apps": {
         "models": {
@@ -14,18 +13,19 @@ TORTOISE_ORM = {
         },
     },
 }
+
+# TORTOISE 
 # seems we want to use Sanic's plugin for tortise-orm so they share the event loop...?
 # async def init_tortise():
 #     await Tortoise.init(
-#         db_url=TORTOISE_ORM["connections"]["default"],
+#         db_url=ORM_CONFIG["connections"]["default"],
 #         modules={'models': ["models"]}
 #     )
 #     # Generate the schema
 #     await Tortoise.generate_schemas()
 
-
-
 # MIGRATION COMMANDS
 # 1. ran 'aerich init-db' to setup our first model (aka an automatically spec'd migraiton file)
 # 2. ran 'aerich migrate --name xyz' to create new migration file
 # 3. ran 'aerich upgrade' to actually run the new migration file
+
