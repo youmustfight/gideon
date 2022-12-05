079f02176b4b591cb2e820c4d89d0ca6842a7a32
Mark <x@markthemark.com>
2022-12-05 10:25:44 -0500

    feat: ai action agent, allowing case-based consistency of model/index usage

---
+++ backends/src/aia/generate_ai_action_locks.py
@@ -0,0 +1,37 @@
+from aia.agent import AI_ACTIONS, AI_MODELS
+from dbs.sa_models import AIActionLock
+
+def generate_ai_action_locks(case_id = None):
+  return [
+    # actions should match a python function name
+    AIActionLock(
+      action=AI_ACTIONS.document_similarity_image_embed.value,
+      model_name=AI_MODELS.ViT_L_14_336px.value,
+      params={},
+      case_id=case_id),
+    AIActionLock(
+      action=AI_ACTIONS.document_similarity_text_sentence_embed.value,
+      model_name=AI_MODELS.all_MiniLM_L6_v2.value,
+      params={},
+      case_id=case_id),
+    AIActionLock(
+      action=AI_ACTIONS.document_similarity_text_max_size_embed.value,
+      model_name=AI_MODELS.text_similarity_curie_001.value,
+      params={},
+      case_id=case_id),
+    AIActionLock(
+      action=AI_ACTIONS.case_similarity_text_sentences_search.value,
+      model_name=AI_MODELS.all_MiniLM_L6_v2.value,
+      params={},
+      case_id=case_id),
+    AIActionLock(
+      action=AI_ACTIONS.case_similarity_text_max_size_search.value,
+      model_name=AI_MODELS.text_similarity_curie_001.value,
+      params={},
+      case_id=case_id),
+    AIActionLock(
+      action=AI_ACTIONS.case_similarity_text_to_image_search.value,
+      model_name=AI_MODELS.ViT_L_14_336px.value,
+      params={},
+      case_id=case_id),
+  ]
