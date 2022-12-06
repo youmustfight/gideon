c14f46d726913798c1b7e6d5757a70934b7e7438
Mark <x@markthemark.com>
2022-12-06 10:49:55 -0500

    feat(perf): ray for distributed compute, using to parallelize locally web requests

--- backends/src/start.py
+++ backends/src/start.py
@@ -1,5 +1,10 @@
 import env
+import ray
 from api import start_api
+
+# Enable Distributed Compute
+# TODO: only works locally, need to setup 'head' and pass address/port
+ray.init()
 
 if __name__ == "__main__":
   start_api()
