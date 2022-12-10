62e944778746af42cb2503f9d756fd1948118ba3
Mark <x@markthemark.com>
2022-12-10 17:36:55 -0500

    feat: paralel services

--- backends/src/start.py
+++ backends/src/start.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 import env
 import torch
 from api import start_api
@@ -10,8 +11,25 @@ from worker import start_worker
 torch.set_num_threads(1)
 
 if __name__ == "__main__":
-  # V2 (better than multi-process bc eats extra cpu + background processes don't cause container to exit)
-  if (env.env_target_service() == 'api'):
-    start_api()
-  if (env.env_target_service() == 'worker'):
-    start_worker()
+    # # V2 SERVICES (better than multi-process bc eats extra cpu + background processes don't cause container to exit)
+    # if (env.env_target_service() == 'api'):
+    #   start_api()
+    # if (env.env_target_service() == 'worker'):
+    #   start_worker()
+
+    # # v3 POOLS for PARALLEL PROCESSES (doesn't work. port isn't being exposed for some reason?)
+    # def exec_iter(func):
+    #     func()
+    # with Pool() as pool:
+    #     pool.starmap_async(exec_iter, [
+    #         start_worker,
+    #         start_api,
+    #     ])
+
+    # v4 PROCESSES (start api + worker, only wait for worker to resolve, that way if it errors, i think the whole container stops?)
+    p1 = Process(target=start_api)
+    p2 = Process(target=start_worker)
+    p1.start()
+    p2.start()
+    # --- block till return (if worker returns re-start. load balancer health check will check api)
+    p2.join()
