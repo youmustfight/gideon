7e72abcb69d375e827e2acabf888a1d180edb6a0
Mark <x@markthemark.com>
2022-12-11 23:20:28 -0500

    refactor: index_document_audio

--- backends/src/indexers/processors/job_index_document_audio.py
+++ backends/src/indexers/processors/job_index_document_audio.py
@@ -1,12 +1,12 @@
 from dbs.sa_sessions import create_sqlalchemy_session
-from indexers.index_audio import index_audio
+from indexers.index_document_audio import index_document_audio
 from indexers.utils.upsert_document_content_vectors import upsert_document_content_vectors
 
-async def job_index_audio(document_id):
+async def job_index_document_audio(document_id):
     session = create_sqlalchemy_session()
     # --- process embeddings/extractions
     async with session.begin():
-        document_id = await index_audio(session=session, document_id=document_id)
+        document_id = await index_document_audio(session=session, document_id=document_id)
     # --- queue indexing
     await upsert_document_content_vectors(session=session, document_id=document_id)
     # --- return
